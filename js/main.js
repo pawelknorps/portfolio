8af214c0724abee3dca9403920c97764b6f41aa9
pawel <48882583+pawo161@users.noreply.github.com>
2019-11-01 18:48:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
   // Preloader
   $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
+      $('#preloader').delay(20).fadeOut('slow', function () {
         $(this).remove();
       });
     }
